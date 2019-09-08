e050ddcedc2953ca9b0cf89a376c21c2d6789fa5
y <597985317@qq.com>
2019-09-08 13:32:49 +0800

    add catch recover

--- service/service.go
+++ service/service.go
@@ -33,6 +33,16 @@ type Weibo struct {
 }
 
 func Have() {
+	//优先使用错误拦截 在错误出现之前进行拦截 在错误出现后进行错误捕获
+	//错误拦截必须配合defer使用  通过匿名函数使用
+	defer func() {
+		//恢复程序的控制权
+		err := recover()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
+
 	normalLogging.Logger.Info("run weibo hava")
 	req := curl.NewRequest()
 	resp, err := req.
